a6840c4bbdc65dbde2f4fe93c77f204cc31fccfd
Cormorant Programming <cormorant.programming@gmail.com>
2022-08-26 09:05:32 +0700

    mongoose

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
 const app = express();
+
+mongoose.connect('mongodb+srv://ModernCoding:Per180h+@cluster0.pptyhei.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
+
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
